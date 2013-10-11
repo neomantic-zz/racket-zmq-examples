7c67aff5c041bdd900d5f66178ccaf468fabdf79
Chad A <calbers@neomantic.com>
2013-10-11 09:15:12 +0900

    no-working edits

--- gistfile1.rkt
+++ gistfile1.rkt
@@ -1,36 +1,49 @@
 #lang racket
 
+(require (prefix-in zmq (planet jaymccarthy/zeromq:2:1)))
 (require (planet jaymccarthy/zeromq:2:1))
 (require ffi/unsafe)
 
-(define (responder)
+(define responder
   (thread (lambda ()
-           (let* ([context (zmq:context 1)]
-                  [socket (zmq:socket context 'REP)])
-             (zmq:socket-bind! socket "tcp://127.0.0.1:1337")
-             (let listen ([listening #t])
-               (let* ([port (open-input-bytes (zmq:socket-recv! socket))]
-                      [rep-bytes (string->bytes/utf-8 (string-append (bytes->string/utf-8 (port->bytes port)) " - echoed!"))]
-                       [a-ptr (malloc zmq:_msg)])
-                   (ptr-set! a-ptr zmq:_msg rep-bytes)
-                   (zmq:socket-send-msg! socket a-ptr 'NOBLOCK)
-                   (close-input-port port))
-               (listen #t))))))
+            (define (send-response socket request-bytes)
+              (define message (malloc _msg 'raw))
+              (set-cpointer-tag! message 'msg-tag)
+              (let* ([response-string (string-append (bytes->string/utf-8 ) " - echoed!")]
+                     [response-bytes (string->bytes/utf-8 response-string)]
+                     [length (bytes-length response-bytes)])
+                (memcpy (msg-data-pointer message) response-bytes length)
+                (dynamic-wind
+                  void
+                  (lambda () (socket-send-msg! socket 'NOBLOCK))
+                  (lambda ()
+                    (msg-close! message)
+                    (free message)))))
+            (let* ([context (context 1)]
+                   [socket (socket context 'REP)])
+              (socket-bind! socket "tcp://127.0.0.1:1337")
+              (let listen ([listening #t])
+                (let* ([port (open-input-bytes (socket-recv! socket))])
+                  (send-response (port->bytes port))
+                  (close-input-port port))
+                (listen #t))))))
 
-(define (requester)
+(define requester
   (thread
-   (lambda ()
-     (let* ([context (zmq:context 1)]
-            [socket (zmq:socket context 'REQ)])
-       (zmq:socket-connect! "tcp://127.0.0.1:1337")
-       (let send-requests ([times 5])
-         (if (< times 0)
-             #t
-             (begin
-               (zmq:socket socket (string->bytes/utf-8 "Hello"))
-               (send-requests (- times 1)))))
-       (let show-responses ([listening #t])
-         (let* ([port (open-input-bytes (zmq:socket-recv! socket))])
-           (display (bytes->string/utf-8 (port->bytes port)))
-           (close-input-port port))
-         (show-responses #t))))))
+    (lambda ()
+      (let* ([context (context 1)]
+             [socket (socket context 'REQ)])
+        (socket-connect! "tcp://127.0.0.1:1337")
+        (define (send-requests times)
+          (if (eq? times 0)
+              (begin
+                (display "done sending"))
+              (socket socket (string->bytes/utf-8 "Hello")))
+          (send-requests (- times 1)))
+        (let show-responses ([listening #t])
+          (let* ([port (open-input-bytes (socket-recv! socket))])
+            (display (bytes->string/utf-8 (port->bytes port)))
+            (close-input-port port)
+            (socket-close! socket))
+          (show-responses #t))
+        (send-requests 5)))))
