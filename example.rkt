6660f41e78add3384a81122d2381d9163ed5de54
Chad A <calbers@neomantic.com>
2013-10-20 13:11:10 +0900

    updated example...it works using places, but it doesn't work with the 'NOBLOCK receive

--- example.rkt
+++ example.rkt
@@ -1,15 +1,14 @@
 #lang racket
 
-(require ffi/unsafe
-;; local path, but use the 'msg-creation-defs' branch
-;; from git@github.com:neomantic/zeromq.git
-;; it supports creating messages
+;; run this with racket -tm example.rkt
+
+(provide main)
+(require racket/place
+         racket/place/distributed
+         ffi/unsafe
          (prefix-in zmq: "../zeromq/net/zmq.rkt"))
 
-;; this half works with my hacked version of zmq
-;; butt hits an error
-
-(define uri "tcp://127.0.0.1:1337")
+(define uri "tcp://127.0.0.1:1344")
 
 ;; this is a literally a copy-paste from zmq.rks
 ;; sock-recv! procedure, with only the 'NOBLOCK
@@ -40,72 +39,87 @@
 (define (zmq-recv-empty socket)
   (zmq:socket-recv! socket))
 
-;; responder
-(thread
- (lambda ()
-   (let* ([context (zmq:context 1)]
-          [socket (zmq:socket context 'REP)])
-     (zmq:socket-bind! socket uri)
-     (define (make-response-bytes recv-bytes)
-       (string->bytes/utf-8
-        (string-append (bytes->string/utf-8 recv-bytes) " - echoed!")))
-     (define (printf-recvd recv-bytes)
-       (printf (string-append
-                "Received Data: "
-                (bytes->string/utf-8 recv-bytes)
-                "\n")))
-     (let listen ()
-       (printf "responder-listening\n")
-       ;; this let works, in the sense that that the data is received
-       ;; and printed
-       (let ([recv-bytes (zmq-recv-empty socket)])
-         (printf-recvd recv-bytes)
-         (zmq-send-noblock socket (make-response-bytes recv-bytes))
-         (printf "responder-responded\n"))
+;; Creates a context and passes it to the func
+;; when the function returns, it closes the context
+(define (call-with-context func)
+  (let ([context (zmq:context 1)])
+    (dynamic-wind
+      void
+      (lambda ()
+        (func context) (void))
+      (lambda ()
+        (zmq:context-close! context)))))
 
-       ;; this let fails, no message is received and printed
-       ;; (let ([recv-bytes (zmq-recv-noblock socket)])
-       ;;   (printf-recvd recv-bytes)
-       ;;   (zmq-send-noblock socket (make-response-bytes recv-bytes))
-       ;;   (printf "responder-responded\n"))
+;; Creates a context and passes it to the func
+;; when the function returns, it closes the context
+(define (call-with-socket context type func )
+  (let ([socket (zmq:socket context type)])
+    (dynamic-wind
+      void
+      (lambda ()
+        (func socket) (void))
+      (lambda ()
+        (zmq:socket-close! socket)))))
 
-       (listen))
-     (zmq:socket-close! socket)
-     (zmq:context-close! context))))
 
-;; requester
-(thread
- (lambda ()
-   (let* ([context (zmq:context 1)]
-          [socket (zmq:socket context 'REQ)])
-	 (zmq:socket-connect! socket uri)
-	 (define (make-request-bytes count)
-	   (string->bytes/utf-8
-		(string-append
-		 "Hello, "
-		 (number->string count))))
-	 (define (printf-response recv-bytes)
-	   (printf (string-append (bytes->string/utf-8 recv-bytes) "\n")))
-     (for ([count 5])
-	   (printf "requester-sending\n")
-	   (zmq-send-noblock socket (make-request-bytes count))
-	   (printf "requester-receiving\n")
-	   (let ([rcvd (zmq-recv-noblock socket)])
-		 (printf-response rcvd)))
-	 (zmq:socket-close! socket)
-	 (zmq:context-close! context))))
+(define (make-request-bytes count)
+          (string->bytes/utf-8
+           (string-append
+            "Hello, "
+            (number->string count))))
 
-(sleep 10)
+(define (make-response-bytes recv-bytes)
+  (string->bytes/utf-8
+   (string-append (bytes->string/utf-8 recv-bytes) " - echoed!")))
 
-;;; output
-;; #<thread:...6f81/example.rkt:42:1>
-;; #<thread:...6f81/example.rkt:75:1>
-;; requester-sending
-;; requester-receiving
-;; socket-recv-msg!: Bad address
-;;   context...:
-;;    /home/neomantic/src/mine/7e687d75d9227ef16f81/example.rkt:75:1
-;; responder-listening
-;; Received Data: Hello, 0
-;; responder-responded
-;; responder-listening
+(define (printf-response recv-bytes)
+          (printf/f (string-append (bytes->string/utf-8 recv-bytes) "\n")))
+
+(define (printf-recvd recv-bytes)
+  (printf/f (string-append
+             "Received Data: "
+             (bytes->string/utf-8 recv-bytes)
+             "\n")))
+
+(define (request)
+  (call-with-context
+   (lambda (context)
+     (call-with-socket
+      context 'REQ
+      (lambda (socket)
+        (zmq:socket-connect! socket uri)
+        (for ([count 5])
+          (printf "requester-sending\n")
+          (zmq-send-noblock socket (make-request-bytes count))
+          (printf "requester-receiving\n")
+          (let ([rcvd (zmq-recv-empty socket)])
+            (printf-response rcvd))))))))
+
+(define (respond)
+  (call-with-context
+   (lambda (context)
+     (call-with-socket
+      context 'REP
+      (lambda (socket)
+        (zmq:socket-bind! socket uri)
+        (let listen ()
+          (printf/f "responder-listening\n")
+          (let ([recv-bytes (zmq-recv-empty socket)])
+            (printf-recvd recv-bytes)
+            (zmq-send-noblock socket (make-response-bytes recv-bytes))
+            (printf/f "responder-responded\n"))
+          ;; this let fails, no message is received and printed
+          ;; (let ([recv-bytes (zmq-recv-noblock socket)])
+          ;;   (printf-recvd recv-bytes)
+          ;;   (zmq-send-noblock socket (make-response-bytes recv-bytes))
+          ;;   (printf "responder-responded\n"))
+          (listen)))))))
+
+(define (main)
+  (place
+     ch
+     (request))
+  (place-channel-get
+   (place
+    ch1
+    (respond))))
