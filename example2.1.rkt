5c1e998349dd805e52d1f0ba111bb9696dd3b421
Chad A <calbers@neomantic.com>
2013-10-24 20:38:38 +0900

    updated concurrent replier example

--- example2.1.rkt
+++ example2.1.rkt
@@ -9,18 +9,24 @@
          "zmq-support.rkt"
          (prefix-in zmq: "../zeromq/net/zmq.rkt"))
 
+;; since this is a zmq inproc transfer, the context must be
+;; shared
+
 (define (proxy-p)
   (place
    p
+   (define (worker-channels-put-context worker-channels context)
+     (for-each (lambda (channel)
+                 (place-channel-put channel context))
+               worker-channels))
    (call-with-context
     (lambda (context)
       (let ([router-socket (zmq:socket context 'ROUTER)]
             [dealer-socket (zmq:socket context 'DEALER)])
-        (zmq:socket-bind! router-socket "tcp://127.0.0.1:1337")
+        (zmq:socket-bind! router-socket "tcp://*:1337")
         (zmq:socket-bind! dealer-socket "inproc://responders")
-        ;; get the worker place
-        ;; send the worker place, the context - so it's shared
-        (place-channel-put (place-channel-get p) context)
+        ;; get the worker places
+        (worker-channels-put-context (place-channel-get p) context)
         (dynamic-wind
           void
           (lambda ()
@@ -32,6 +38,7 @@
             (zmq:socket-close! dealer-socket))))))))
 
 (define (worker-p)
+  (printf/f "defining worker\n")
   (place
    w
    (call-with-socket
@@ -39,6 +46,8 @@
     (place-channel-get w)
     'REP
     (lambda (socket)
+      ;; Just a note - there seems to be a bug in 0mq where socket connect has to be
+      ;; called before socket bind for inproc
       (zmq:socket-connect! socket "inproc://responders")
       (let listen ()
         (printf/f "worker-listening\n")
@@ -47,9 +56,13 @@
           (zmq:socket-send! socket (make-response-bytes recv-bytes)))
         (listen))))))
 
+(define (workers-list count)
+  (for/fold ([workers '()])
+      ([i count])
+    (append workers (list (worker-p)))))
+
 (define (main)
-  (let ([worker (worker-p)]
-        [proxy  (proxy-p)])
-    ;; send the proxy the worker, so the proxy can send the worker the context
-    (place-channel-put proxy worker)
+  (let ([proxy (proxy-p)])
+    ;; send the proxy the workers, so the proxy can send the worker the context
+    (place-channel-put proxy (workers-list 5))
     (place-channel-get proxy)))
