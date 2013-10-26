14d65815567ceb4063c248f19f1a9aeaeee0a085
Chad A <calbers@neomantic.com>
2013-10-26 21:20:28 +0900

    more consider requester-router example

--- request-router.rkt
+++ request-router.rkt
@@ -10,59 +10,42 @@
          (planet zitterbewegung/uuid-v4:2:0/uuid-v4)
          (prefix-in zmq: "../zeromq/net/zmq.rkt"))
 
-(define (make-proxy-place)
-  (printf/f "defining proxy\n")
-  (place
-   proxy-channel
-   (define worker-url (string-append "inproc://" (string-downcase (symbol->string (make-uuid)))))
-   ;; send each worker the context and the url
-   (define server-url "tcp://127.0.0.1:1337")
-   (call-with-context (lambda (context)
-     (call-with-router-dealer-sockets context
-      (lambda (router-socket dealer-socket)
-        (printf/f "connecting dealer of proxy: ~a\n" server-url)
-        (zmq:socket-connect! router-socket server-url)
-        (printf/f "binding router of proxy: ~a\n" worker-url)
-        (zmq:socket-bind! dealer-socket worker-url)
-        (let notify-workers ([workers (place-channel-get proxy-channel)]
-                   [count 0])
-          (if (empty? workers)
-            #f
-            (begin
-              ;; send each worker their id,
-              ;; their context and their url
-              (place-channel-put
-               (car workers)
-               (list (+ count 1) context worker-url))
-              (notify-workers (cdr workers) (+ count 1)))))))))))
+
+(define server-url "tcp://127.0.0.1:1337")
+(provide (all-defined-out))
 
 (define (make-worker-place)
   (printf/f "defining worker\n")
   (place
    worker-channel
    ;; block until we receive the context, the url, and worker number from the proxy
-   (let* ([context-and-url (place-channel-get worker-channel)]
-          [context (cadr context-and-url)]
-          [url (caddr context-and-url)]
-          [worker-number (number->string (car context-and-url))])
+   (let ([context-and-url (place-channel-get worker-channel)])
      (call-with-req-socket
-      context
-      (lambda (socket)
-        (printf/f "connecting worker to ~a\n" url)
-        (zmq:socket-connect! socket url)
-        (for ([count 100000])
-          (printf "requester-sending\n")
-          (zmq:socket-send! socket (make-request-bytes worker-number count))
-          (printf "requester-receiving\n")
-          (let ([recv-bytes (zmq:socket-recv! socket)])
-            (printf-recvd recv-bytes))))))))
-
-(define (make-workers count)
-  (for/fold ([workers '()])
-      ([i count])
-    (append workers (list (make-worker-place)))))
+      (car context-and-url)
+     (lambda (socket)
+       (printf/f "connecting worker to ~a\n" (cadr context-and-url))
+       (zmq:socket-connect! socket (cadr context-and-url))
+       (for ([count 100000])
+         (printf "requester-sending\n")
+         (zmq:socket-send! socket (make-request-bytes "number" count))
+         (printf "requester-receiving\n")
+         (let ([recv-bytes (zmq:socket-recv! socket)])
+           (printf-recvd recv-bytes))))))))
 
 (define (main)
-  (let ([proxy-place (make-proxy-place)])
-    (place-channel-put proxy-place (make-workers 5))
-    (place-channel-get proxy-place)))
+  (printf/f "defining proxy\n")
+  (place-wait
+     (place
+      proxy-channel
+      (define worker-url (string-append "inproc://" (string-downcase (symbol->string (make-uuid)))))
+      (call-with-context
+       (lambda (context)
+         (call-with-router-dealer-sockets
+          context
+          (lambda (router-socket dealer-socket)
+            (printf/f "connecting dealer of proxy: ~a\n" server-url)
+            (zmq:socket-connect! router-socket server-url)
+            (printf/f "binding router of proxy: ~a\n" worker-url)
+            (zmq:socket-bind! dealer-socket worker-url)
+            (for ([count 5])
+              (place-channel-put (make-worker-place) (list context worker-url))))))))))
